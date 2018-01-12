805b834ee55c13961d33232a53a7856aa3dd984a
mamt84 <mamt84@gmail.com>
2018-01-11 20:53:07 -0500

    Initial commit

---
+++ src/main/java/com/example/courserism/CourserismApplication.java
@@ -0,0 +1,12 @@
+package com.example.courserism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourserismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourserismApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/courserism/CourserismApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.courserism;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourserismApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
